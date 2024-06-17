9c02e2b276d16436444780784799fed5c6f5f895
kernenyabut <kernenyabut@gmail.com>
2024-06-17 17:52:46 -0400

    Added some missing features.
    Added:
    > Sticky navbar
    > Contact button
    > Copyright information
    > Favicons
    > 404 page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 import '../styles/main.css';
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function EnableMobileMenu() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
