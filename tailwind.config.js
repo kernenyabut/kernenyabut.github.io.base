a29c209c882210b6de9623af0ecc6d0f14bf3af6
kernenyabut <kernenyabut@gmail.com>
2024-07-26 15:02:49 -0400

    Attempt at a lander, revamped life story.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,5 @@ module.exports = {
     "./src/blog/**/*.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}",
     "./src/pages/**/*.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}",
     "./src/index.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}",
-  ]
+  ],
 }
