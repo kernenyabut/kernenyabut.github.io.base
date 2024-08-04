b13d70a44f8079531f30e0914c76b6e61e6dc536
kernenyabut <kernenyabut@gmail.com>
2024-08-04 16:57:29 -0400

    Minor "sprinkles" in between, while I try to fix the lander image.

---
+++ src/scripts/intro.js
@@ -0,0 +1,9 @@
+console.log(`
+    _   _      _ _
+    | | | |    | | |
+    | |_| | ___| | | ___
+    |  _  |/ _ \\ | |/ _ \\
+    | | | |  __/ | | (_) |
+    \\_| |_/\\___|_|_|\\___/
+  `);
+console.log("I see that you are taking a little sneak peek at inner workings of my site. Nice of you to go beyond the front-end. Surf along!");
