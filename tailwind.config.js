487a79f7eaf4ce4f056c542e302b6592ab80e688
kernenyabut <kernenyabut@gmail.com>
2024-08-22 15:11:31 -0400

    Added more personalization (mainly borders/colours). Added basic lander page.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,4 +6,15 @@ module.exports = {
     "./src/pages/**/*.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}",
     "./src/index.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}",
   ],
+  theme: {
+    extend: {
+      textUnderlineOffset: {
+        3: '3px',
+        10: '10px',
+        12: '12px',
+        14: '14px',
+        16: '16px',
+      }
+    }
+  }
 }
