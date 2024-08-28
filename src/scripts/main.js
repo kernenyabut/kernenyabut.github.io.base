81e8e32e72c69045b1039e18c32ba43f2dde4cdd
kernenyabut <kernenyabut@gmail.com>
2024-08-28 19:00:48 -0400

    Added basic "sprinkles" - little animations/tidbits to personalize.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,12 @@
 import '../styles/main.css';
 
+// Attach the function to the button's click event
+document.getElementById("toggleButton").style.display='block';
+document.getElementById("toggleButton").addEventListener("click", EnableMobileMenu);
+
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function EnableMobileMenu() {
-    var infoBlock = document.getElementById("myLinks");
+    var infoBlock = document.getElementById("collapsibleContent");
     var symbol = document.getElementById('toggleSymbol');
 
     if (infoBlock.style.display === "block") {
@@ -16,4 +20,3 @@ function EnableMobileMenu() {
     }
   }
 
-// Attach the function to the button's click event
